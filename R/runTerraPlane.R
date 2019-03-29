b051c2abbfa4b80483ccf16e5eb718735d7285c6
shwetagopaul <shwetagopaul92@gmail.com>
2019-03-29 15:26:58 -0400

    changes to filename

---
+++ R/runTerraPlane.R
@@ -0,0 +1,11 @@
+######## Shiny APP ########
+### TerraPlane ###
+
+#'to help filter dockstore to find methods
+#'@import shiny
+#'@export
+runTerraPlane<-function(){
+  myfile = system.file("shinyApps/TerraPlaneApp.R",package="TerraPlane")
+  source(myfile)
+  TerraPlane()
+}
